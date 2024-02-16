09a330b7ec55bc34c8c30adf13d6a83d013b88d4
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-02-16 12:41:41 +0530

    add pip functions and adjustments in java

--- src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/GameActivity.java
+++ src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/GameActivity.java
@@ -12,6 +12,8 @@ import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
+import android.app.PictureInPictureParams;
+import android.content.res.Configuration;
 import com.panda3ds.pandroid.AlberDriver;
 import com.panda3ds.pandroid.R;
 import com.panda3ds.pandroid.app.game.AlberInputListener;
@@ -73,10 +75,45 @@ public class GameActivity extends BaseActivity {
 		InputHandler.reset();
 		InputHandler.setMotionDeadZone(InputMap.getDeadZone());
 		InputHandler.setEventListener(inputListener);
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    setPictureInPictureParams(new PictureInPictureParams.Builder().setAutoEnterEnabled(true).build());
+		 }
+		
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
 			getTheme().applyStyle(R.style.GameActivityNavigationBar, true);
 		}
 	}
+
+	@Override
+        public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+        if (isInPictureInPictureMode) {
+           findViewById(R.id.overlay_controller).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.overlay_controller).setVisibility(View.VISIBLE);
+        }
+    }
+
+	@Override
+        public void onUserLeaveHint() {
+        super.onUserLeaveHint();
+	DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int widthPixels = displayMetrics.widthPixels;
+        int heightPixels = displayMetrics.heightPixels;
+
+        // Calculate aspect ratio
+        float aspectRatio = (float) widthPixels / (float) heightPixels;
+		
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            Rational aspectRatioRational = new Rational(widthPixels, heightPixels);
+            PictureInPictureParams.Builder pipBuilder = new PictureInPictureParams.Builder();
+            pipBuilder.setAspectRatio(aspectRatioRational);
+            enterPictureInPictureMode(pipBuilder.build());
+        }
+    }
 
 	@Override
 	protected void onPause() {
