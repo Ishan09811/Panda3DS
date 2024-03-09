bee497820f8d676832a19b262bf6c246e1a5c5b5
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-03-09 13:30:32 +0530

    bonk

--- src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/main/SettingsFragment.java
+++ src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/main/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
+import android.widget.Toast;
 import androidx.core.content.FileProvider;
 
 import androidx.annotation.Nullable;
