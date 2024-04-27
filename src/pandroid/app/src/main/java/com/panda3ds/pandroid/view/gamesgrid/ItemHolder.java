5319700ee0d54d7532ab83005442360a31faac5b
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-04-27 12:14:34 +0530

    fix

--- src/pandroid/app/src/main/java/com/panda3ds/pandroid/view/gamesgrid/ItemHolder.java
+++ src/pandroid/app/src/main/java/com/panda3ds/pandroid/view/gamesgrid/ItemHolder.java
@@ -13,19 +13,19 @@ import com.panda3ds.pandroid.data.game.GameMetadata;
 class ItemHolder extends RecyclerView.ViewHolder {
     public ItemHolder(@NonNull View itemView) {
         super(itemView);
-    }
 
-    new Handler().postDelayed(new Runnable() {
-    @Override
-    public void run() {
-        // Set the ellipsize property and make the TextViews selectable to start the marquee
-        ((AppCompatTextView) itemView.findViewById(R.id.title)).setEllipsize(TextUtils.TruncateAt.MARQUEE);
-        ((AppCompatTextView) itemView.findViewById(R.id.title)).setSelected(true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Set the ellipsize property and make the TextViews selectable to start the marquee
+                ((AppCompatTextView) itemView.findViewById(R.id.title)).setEllipsize(TextUtils.TruncateAt.MARQUEE);
+                ((AppCompatTextView) itemView.findViewById(R.id.title)).setSelected(true);
 
-        ((AppCompatTextView) itemView.findViewById(R.id.description)).setEllipsize(TextUtils.TruncateAt.MARQUEE);
-        ((AppCompatTextView) itemView.findViewById(R.id.description)).setSelected(true);
+                ((AppCompatTextView) itemView.findViewById(R.id.description)).setEllipsize(TextUtils.TruncateAt.MARQUEE);
+                ((AppCompatTextView) itemView.findViewById(R.id.description)).setSelected(true);
+            }
+        }, 3000);
     }
-}, 3000);
 
     public void apply(GameMetadata game) {
         ((AppCompatTextView) itemView.findViewById(R.id.title))
