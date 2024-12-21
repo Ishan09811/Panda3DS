f01645a401288b94fe26191da5299ed19a206ecd
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-12-21 11:31:15 +0530

    Update GameActivity.java

--- src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/GameActivity.java
+++ src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/GameActivity.java
@@ -21,6 +21,7 @@ import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
